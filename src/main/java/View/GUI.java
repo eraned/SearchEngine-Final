23cd432d38c76e9dbe82f1655e944fa450e54d21
Eran Edri <eranedrimail@gmail.com>
2018-12-16 15:31:23 +0200

    eran - part B first push

--- src/main/java/View/GUI.java
+++ src/main/java/View/GUI.java
@@ -1,19 +1,21 @@
 package View;
 
 import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
-        import java.io.IOException;
-        import java.net.URISyntaxException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 
 public class GUI extends Application {
-
+    public static Stage MainStage;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("GUIStructure.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        MainStage = primaryStage;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("GUIStructure.fxml")));
         primaryStage.setTitle("Welcom to the best search engine in the world!!");
         primaryStage.setScene(new Scene(root, 500, 700));
         primaryStage.show();
